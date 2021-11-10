42ef63692d48da4f5aba31fb7c69d42be83714e1
jaeyoung <manatom55@gmail.com>
2021-11-10 14:56:31 +0000

    make train.py file

--- generator/dataset.py
+++ generator/dataset.py
@@ -1,7 +1,6 @@
 from torch.utils.data import Dataset
 import pandas as pd
 from kobart import get_kobart_tokenizer
-
 def add_to(sentenct,bos_token='<s>',eos_token='</s>'):
     new_sentence = bos_token + sentenct + eos_token
     return new_sentence
@@ -14,17 +13,24 @@ class wellnessDataset(Dataset):
         self.eos_token = self.tokenizer.eos_token
         csv_file = pd.read_csv(path)
         self.quesion = csv_file['Q'].map(add_to)
+        self.quesion = self.tokenizer(self.quesion.tolist(),padding=True,return_tensors='pt')
+
         self.answer = csv_file['A'].map(add_to)
+        self.answer = self.tokenizer(self.answer.tolist(),padding=True,return_tensors='pt')
         print(self.quesion)
 
     def __len__(self):
-        return len(self.answer)
+        return len(self.answer['input_ids'])
 
     def __getitem__(self, idx):
-        question = self.tokenizer(self.quesion[idx])
-        answer = self.tokenizer(self.answer[idx])
+
+        question = {i:v[idx] for i,v in self.quesion.items()}
+        answer = {i:v[idx] for i,v in self.answer.items()}
+        labels = answer['input_ids'][:]
         return {'input_ids':question['input_ids'],'attention_mask':question['attention_mask'],
-                'decoder_input_ids':answer['input_ids'],'decoder_attention_mask':answer['attention_mask']}
+                'decoder_input_ids':answer['input_ids'],'decoder_attention_mask':answer['attention_mask'],
+                'labels':labels}
+
 
 if __name__ == '__main__':
     x = wellnessDataset('../Chatbot_data/ChatbotData.csv')
@@ -32,4 +38,7 @@ if __name__ == '__main__':
     p = x[1]
     tokenizer = get_kobart_tokenizer()
     print(tokenizer.decode(p['input_ids']))
+    print(p['decoder_input_ids'])
+    print(p['labels'])
     print(tokenizer.decode(p['decoder_input_ids'], skip_special_tokens=True))
+    print(tokenizer.decode(p['labels'], skip_special_tokens=True))
--- generator/train.py
+++ generator/train.py
@@ -1,22 +1,57 @@
 """ko-bart로 단순 generator"""
-from transformers import BartForConditionalGeneration,trainer
+from transformers import BartForConditionalGeneration,Seq2SeqTrainingArguments,DataCollatorWithPadding,Seq2SeqTrainer
 from kobart import get_pytorch_kobart_model, get_kobart_tokenizer
 from dataset import wellnessDataset
-
-
+import torch
 def train(args):
     model = BartForConditionalGeneration.from_pretrained(get_pytorch_kobart_model())
-
-
+    dataset = wellnessDataset('../Chatbot_data/ChatbotData.csv')
+    tokenizer = get_kobart_tokenizer()
+    print(len(dataset))
+    x = int(len(dataset) * 0.8)
+    train_set, val_set = torch.utils.data.random_split(dataset, [len(dataset)-x,x])
+    training_args = Seq2SeqTrainingArguments(
+        output_dir="./results",  # output directory
+        save_total_limit=5,  # number of total save model.
+        save_steps=args.save_steps,  # model saving step.
+        num_train_epochs=args.num_train_epochs,  # total number of training epochs
+        learning_rate=args.learning_rate,  # learning_rate
+        per_device_train_batch_size=args.batch_size,  # batch size per device during training
+        per_device_eval_batch_size=32,  # batch size for evaluation
+        warmup_steps=args.warmup_steps,  # number of warmup steps for learning rate scheduler
+        weight_decay=args.weight_decay,  # strength of weight decay
+        logging_dir="./logs",  # directory for storing logs
+        logging_steps=200,  # log saving step.
+        evaluation_strategy="steps",  # evaluation strategy to adopt during training
+        # `no`: No evaluation during training.
+        # `steps`: Evaluate every `eval_steps`.
+        # `epoch`: Evaluate every end of epoch.
+        eval_steps=200,  # evaluation step.
+        load_best_model_at_end=True,
+        predict_with_generate= True,
+    )
+    data_collator = DataCollatorWithPadding(
+        tokenizer, pad_to_multiple_of=16)
+    trainer = Seq2SeqTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_set,
+        eval_dataset=val_set ,
+        tokenizer=tokenizer,
+        # data_collator=data_collator
+)
+    trainer.train()
+    trainer.save_model()
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description="Process some integers.")
-    parser.add_argument("--batch_size", type=int, default=22, help="model name")
+    parser.add_argument("--batch_size", type=int, default=16, help="model name")
     parser.add_argument("--device", type=str, default="cuda", help="device")
     parser.add_argument("--epochs", type=int, default=1, help="epochs")
     parser.add_argument("--learning_rate", type=float, default=10e-5, help="dataset")
+    parser.add_argument("--save_steps", type=int, default=200, help="dataset")
+    parser.add_argument("--num_train_epochs", type=int, default=3, help="dataset")
     parser.add_argument("--warmup_steps", type=int, default=500, help="dataset")
     parser.add_argument("--weight_decay", type=float, default=0.01, help="dataset")
-
     args = parser.parse_args()
     train(args)
